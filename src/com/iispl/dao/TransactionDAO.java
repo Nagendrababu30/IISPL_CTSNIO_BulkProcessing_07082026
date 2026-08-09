16f4402702e220aaab1a63c6f03f6a71ceabf563
Dell <pamisettipoojitha2003@gmail.com>
2026-08-09 11:54:37 +0530

    Implemented saveProduct functionality

--- src/com/iispl/dao/TransactionDAO.java
+++ src/com/iispl/dao/TransactionDAO.java
@@ -4,8 +4,6 @@ import com.iispl.model.TransactionRequest;
 import com.iispl.model.TransactionResult;
 
 public interface TransactionDAO {
-
-	public boolean getTransactionById(String transactionId);
 	
 	public boolean saveTransaction(TransactionRequest request, TransactionResult result);
 	
--- src/com/iispl/dao/TransactionDAOImpl.java
+++ src/com/iispl/dao/TransactionDAOImpl.java
@@ -1,19 +1,45 @@
 package com.iispl.dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+
+import com.iispl.connectionpool.ConnectionPool;
 import com.iispl.model.TransactionRequest;
 import com.iispl.model.TransactionResult;
 
 public class TransactionDAOImpl implements TransactionDAO {
 
 	@Override
-	public boolean getTransactionById(String transactionId) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
 	public boolean saveTransaction(TransactionRequest request, TransactionResult result) {
-		// TODO Auto-generated method stub
+		String insertSql = """
+				INSERT INTO transaction
+				(transaction_id, batch_id, from_account, to_account,
+				 transaction_amount, transaction_type, source_file,
+				 remarks, status, processing_date)
+				VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+				""";
+		try (Connection connection = ConnectionPool.getDataSource().getConnection();
+				PreparedStatement prepStmt = connection.prepareStatement(insertSql)) {
+			prepStmt.setString(1, request.getTransactionId());
+			prepStmt.setString(2, request.getBatchId());
+			prepStmt.setString(3, request.getFromAccount());
+			prepStmt.setString(4, request.getToAccount());
+			prepStmt.setBigDecimal(5, request.getTransactionAmount());
+			prepStmt.setString(6, request.getTransactionType().toString().toUpperCase());
+			prepStmt.setString(7, result.getSourceFile().toString());
+			prepStmt.setString(8, result.getRemarks());
+			prepStmt.setString(9, result.getStatus().toString());
+			prepStmt.setDate(10,Date.valueOf(result.getProcessingDate()));
+
+			int rowsAffected = prepStmt.executeUpdate();
+			if (rowsAffected > 0) {
+				return true;
+			}
+
+		} catch (Exception ex) {
+			ex.getMessage();
+		}
 		return false;
 	}
 
