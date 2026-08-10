7758197f04d468aa68f1236e1d6ec08b8f7969d8
venkat438 <venkateswarareddyd123@gmail.com>
2026-08-10 09:47:40 +0530

    Implemented WriteFailedTransaction Functionality

--- src/com/iispl/nio/ResponseFileWriter.java
+++ src/com/iispl/nio/ResponseFileWriter.java
@@ -1,5 +1,7 @@
 package com.iispl.nio;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import com.iispl.model.FileProcessingSummary;
@@ -22,8 +24,49 @@ public class ResponseFileWriter {
 		
 	}
 	
-	public void writeFailedTransaction(TransactionRequest transactionRequest, TransactionResult transactionResult) {
-		
+	public void writeFailedTransaction(TransactionRequest transactionRequest, TransactionResult transactionResult) throws IOException {
+	    Files.createDirectories(rejectedFolder);
+		Path rejectedFile = rejectedFolder.resolve(fileName);
+		transactionResult.setSourceFile(rejectedFile);
+
+		StringBuilder xml = new StringBuilder();
+
+		xml.append("<Transaction>\n");
+
+		xml.append("<TransactionRequest>\n");
+
+		xml.append("<transactionId>").append(transactionRequest.getTransactionId()).append("</transactionId>\n");
+
+		xml.append("<batchId>").append(transactionRequest.getBatchId()).append("</batchId>\n");
+
+		xml.append(" <fromAccount>").append(transactionRequest.getFromAccount()).append("</fromAccount>\n");
+
+		xml.append(" <toAccount>").append(transactionRequest.getToAccount()).append("</toAccount>\n");
+
+		xml.append("<transactionAmount>").append(transactionRequest.getTransactionAmount())
+				.append("</transactionAmount>\n");
+
+		xml.append("<transactionType>").append(transactionRequest.getTransactionType()).append("</transactionType>\n");
+
+		xml.append("</TransactionRequest>\n");
+
+		xml.append("<TransactionResult>\n");
+
+		xml.append("<transactionId>").append(transactionResult.getTransactionId()).append("</transactionId>\n");
+
+		xml.append("<sourceFile>").append(transactionResult.getSourceFile()).append("</sourceFile>\n");
+
+		xml.append("<remarks>").append(transactionResult.getRemarks()).append("</remarks>\n");
+
+		xml.append("<status>").append(transactionResult.getStatus()).append("</status>\n");
+
+		xml.append(" <processingDate>").append(transactionResult.getProcessingDate()).append("</processingDate>\n");
+
+		xml.append(" </TransactionResult>\n");
+
+		xml.append("</Transaction>");
+
+		Files.writeString(rejectedFile, xml.toString());
 	}
 	
 	public void writeFileSummary(FileProcessingSummary fileProcessingSummary) {
